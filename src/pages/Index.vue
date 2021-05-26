f295b4806d0608d39fdd0ec56b52f88aa2b41c9b
zahiracantika <zahira.19411005@student.ubl.ac.id>
2021-05-26 17:11:59 +0700

    25 Mei 2021

---
+++ src/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="form.namaKegiatan"
+        label="Nama Kegiatan *"
+        :rules="[ val => val && val.length > 0 || 'Mohon Isi Nama Kegiatan']"
+      />
+        <q-input
+        filled
+        v-model="form.deskripsi"
+        label="Deskrpsi Kegiatan *"
+        :rules="[ val => val && val.length > 0 || 'Mohon Isi Deskripsi Kegiatan']"
+      />
+        <q-input
+        filled
+        v-model="form.waktu"
+        label="Waktu Kegiatan *"
+        :rules="[ val => val && val.length > 0 || 'Mohon Isi Waktu Kegiatan']"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+      </div>
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+
+export default{
+  data () {
+    return {
+      form: {
+        namaKegiatan: null,
+        deskripsi: null,
+        waktu: null
+      }
+    } 
+  },
+  methods: {
+    onSubmit () {
+      this.$axios.post('kegiatan/insert', this.form)
+      .then(res => {
+        console.log(res)
+      }).catch(e => {
+            console.log(e)
+          })
+    }
+  },
+}
+</script>
